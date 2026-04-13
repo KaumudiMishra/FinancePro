65fa4743b8950cab37f6aa68864b2d8df21a1190
Kaumudi Mishra <kaumudi1204@gmail.com>
2026-04-13 10:26:38 +0530

    basics and DOM from phase 1

---
+++ scripts.js
@@ -0,0 +1,83 @@
+const expenses = [];
+
+const addExpenseButton = document.querySelector("#addexpense");
+
+addExpenseButton.addEventListener("click", function () {
+  const incomeValue = Number(document.querySelector("#income").value);
+  const amount = Number(document.querySelector("#exp").value);
+  const category = document.querySelector("#category").value;
+  const date = document.querySelector("#date").value;
+
+  addExpense(amount, category, date);
+  calculateTotal(incomeValue);
+  renderExpenses();
+});
+
+function Expense(amount, category, date) {
+  this.amount = amount;
+  this.category = category;
+  this.date = date;
+}
+
+function addExpense(amount, category, date) {
+  expenses.push(new Expense(amount, category, date));
+}
+
+function renderExpenses() {
+  const expenseListContainer = document.getElementById("expense-list");
+  const incomeInput = document.getElementById("income");
+
+  expenseListContainer.innerHTML = "";
+
+  const expenseListTitle = document.createElement("div");
+  expenseListTitle.innerHTML =
+    "<h3>Expense List</h3><p>No. | Date | Expense | Category</p>";
+
+  expenseListContainer.appendChild(expenseListTitle);
+
+  for (let i = 0; i < expenses.length; i++) {
+    const expense = expenses[i];
+
+    const expenseRow = document.createElement("div");
+    expenseRow.textContent = `${i + 1} | ${expense.date} | ${expense.amount} | ${expense.category}`;
+
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML = "Delete";
+
+    deleteButton.addEventListener("click", function () {
+      deleteExpense(i);
+      renderExpenses();
+      calculateTotal(Number(incomeInput.value));
+    });
+
+    expenseRow.appendChild(deleteButton);
+    expenseListContainer.appendChild(expenseRow);
+  }
+}
+
+function calculateTotal(incomeValue) {
+  const totalExpense = expenses.reduce((acc, cur) => acc + cur.amount, 0);
+  const netSavings = incomeValue - totalExpense;
+
+  const totalDisplay = document.getElementById("total-expense-display");
+  totalDisplay.innerHTML = "";
+
+  const summaryDiv = document.createElement("div");
+  summaryDiv.innerHTML = `<p>Total Expense: ${totalExpense}</p><p>Net Savings: ${netSavings}</p>`;
+
+  totalDisplay.appendChild(summaryDiv);
+}
+
+function deleteExpense(index) {
+  expenses.splice(index, 1);
+}
+
+function filterByCategory(category) {
+  for (let expense of expenses) {
+    if (expense.category === category) {
+      console.log(
+        `${expense.amount} | ${expense.category} | ${expense.date}`
+      );
+    }
+  }
+}
